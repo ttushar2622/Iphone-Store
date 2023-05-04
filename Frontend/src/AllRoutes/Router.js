bc832eafc2dcbfaef18b7865efb01fa08d00a47d
JAI RAJ SINGH <jairajsingh28@gmail.com>
2023-05-04 20:37:52 +0530

    check

--- Frontend/src/AllRoutes/Router.js
+++ Frontend/src/AllRoutes/Router.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Homepage from '../pages/Homepage'
 import { Route,Routes } from 'react-router-dom'
+import { Dashboard } from '../admin/dashboard'
+import IpadPage from '../pages/IpadPage'
 
 const Router = () => {
   return (
     <div>
         <Routes>
             <Route path="/" element={<Homepage />} />
-    
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/ipadPage" element={<IpadPage/>} />
         </Routes>
     </div>
   )
