9ac8d0edf6bf47b19553f76f691d5ea6a358859a
ttushar2622 <tthorat22@gmail.com>
2023-05-05 20:25:59 +0530

    iphone

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,11 @@
+// import { Router } from "react-router-dom";
 import { Dashboard } from "./admin/dashboard";
 import NavbarTop from "./component/NavbarTop";
 import Navbarbottom from "./component/Navbarbottom";
 import Homepage from "./pages/Homepage";
 import IpadPage from "./pages/IpadPage";
-
+import Iphone from "./pages/Iphone";
+import Router from "./AllRouths/Router";
 function App() {
   return (
     <div>
@@ -19,7 +21,7 @@ function App() {
         <Navbarbottom />
      
       </div><br/><br/><br/><br/>
-      <Homepage />
+      <Router/>
      </div>
   );
 }
