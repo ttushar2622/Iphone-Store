38c45f67c96a55c8b327ece78eb03c1765c1ca04
sagarbisht07 <bishtsagarbisht1232gmail.com>
2023-05-06 15:49:00 +0530

    now all things are fine

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           zIndex: "100",
         }}>
         <NavbarTop />
-        <Navbarbottom />
+        {/* <Navbarbottom /> */}
       </div>{" "}
       <br /> <br /> <br /> <br />
       <Router />
