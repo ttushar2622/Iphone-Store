3de3b194a96ccbdd952bb8e6cf57170f1191c6a9
ttushar2622 <tthorat22@gmail.com>
2023-05-08 21:32:22 +0530

    change

--- Frontend/src/AllRoutes/Router.js
+++ Frontend/src/AllRoutes/Router.js
@@ -12,6 +12,7 @@ import PaymentPage from "../pages/Payment";
 import Product from "../pages/Product";
 
 
+
 import IpadSinglePage from "../pages/IpadSinglePage";
 
 const Router = () => {
@@ -28,10 +29,9 @@ const Router = () => {
         <Route path="/payment" element={<PaymentPage/>} />
         <Route path="/singlepage/:id" element={<Singleproduct />} />
         <Route path="/product" element={<Product />} />
-        <Route path="*" element={<h1>404 Page Not Found</h1>} /
-
-
-        <Route path="/ipadSinglepage/:id" element={<IpadSinglePage/>} />
+        <Route path="*" element={<h1>404 Page Not Found</h1>}/>
+        <Route path="/ipadSinglepage/:id" element={<IpadSinglePage/>}/>
+        
 
       </Routes>
     </div>
